50408047d657440c937c5500af97014f7e9d0914
DasLampe <daslampe@lano-crew.org>
2019-05-17 15:41:51 +0200

    Update to Gitea 1.8.1
    - Move create user to metadata
    - stop gitlab before update
    - Update app.ini and systemd service to 1.8.1

--- items.py
+++ items.py
@@ -29,29 +29,35 @@ svc_systemd = {
         'needs': [
             'download:/usr/local/bin/gitea',
             'file:/etc/gitea/app.ini',
-            'action:create_gitea_user',
         ],
     },
 }
 
+
 downloads = {
     '/usr/local/bin/gitea': {
-        'url': 'https://dl.gitea.io/gitea/{ver}/gitea-{ver}-linux-amd64' \
-            .format(ver=node.metadata.get('gitea', {}).get('version', '1.4')),
-        'sha256': node.metadata.get('gitea', {}).get('sha256', \
-            '9e4d558c937abe3d16ab57520aaecdd003345fbfc72bb3f52dabe9be739fdad5'),
+        'url': 'https://dl.gitea.io/gitea/{ver}/gitea-{ver}-linux-amd64'.format(
+            ver=node.metadata.get('gitea', {}).get('version', '1.8.1')),
+        'sha256': node.metadata.get('gitea', {}).get('sha256',
+                                                     'cc985b7947cdd50cb47a68bbb51e3a8519d0d2710a9696255928ae9912dca598'
+                                                     ),
+        'preceded_by': [
+            'action:stop_gitea',
+        ]
     },
 }
 
 actions = {
-    'create_gitea_user': {
-        'command': 'useradd -c "Gitea User" -d /var/lib/gitea -m -s /bin/bash {}'.format(user),
-        'unless': 'id -u {}'.format(user),
+    'stop_gitea': {
+        'command': 'systemctl stop gitea',
+        'unless': '[ "`systemctl is-active gitea`" != "active" ]',
+        'triggered': True,
+        'cascade_skip': False,
     },
     'chown_gitea': {
         'command': 'chown {}:{} /usr/local/bin/gitea'.format(user, group),
         'needs': [
-            'action:create_gitea_user',
+            'user:{}'.format(user),
             'download:/usr/local/bin/gitea',
         ],
         'needed_by': [
@@ -61,7 +67,6 @@ actions = {
     'chmod_gitea': {
         'command': 'chmod 0764 /usr/local/bin/gitea',
         'needs': [
-            'action:create_gitea_user',
             'download:/usr/local/bin/gitea',
         ],
         'needed_by': [
@@ -71,58 +76,36 @@ actions = {
 }
 
 directories = {
-    '/var/lib/gitea': {
-        'owner': user,
-        'group': group,
-        'needs': [
-            'action:create_gitea_user',
-        ]
-    },
     '/var/lib/gitea/custom': {
         'owner': user,
         'group': group,
         'owner': user,
-        'needs': [
-            'action:create_gitea_user',
-        ]
     },
     '/var/lib/gitea/data': {
         'owner': user,
         'group': group,
         'mode': '0750',
-        'needs': [
-            'action:create_gitea_user',
-        ]
     },
     '/var/lib/gitea/indexers': {
         'owner': user,
         'group': group,
         'mode': '0750',
-        'needs': [
-            'action:create_gitea_user',
-        ]
     },
     '/var/lib/gitea/public': {
         'owner': user,
         'group': group,
-        'needs': [
-            'action:create_gitea_user',
-        ]
     },
     '/var/lib/gitea/log': {
         'owner': user,
         'group': group,
         'mode': '0750',
-        'needs': [
-            'action:create_gitea_user',
-        ]
     },
     '/etc/gitea': {
         'owner': 'root',
         'group': group,
         'mode': '0750',
         'needs': [
-            'action:create_gitea_user',
+            'user:{}'.format(user),
         ]
     }
 }
@@ -138,9 +121,6 @@ files = {
         'group': group,
         'mode': '0644',
         'unless': 'test -f /etc/gitea/app.ini', # Gitea write INTERNAL_TOKEN to ini file
-        'needs': [
-            'action:create_gitea_user',
-        ]
     },
     '/etc/systemd/system/gitea.service': {
         'source': 'etc/systemd/system/gitea.service',
@@ -158,4 +138,3 @@ files = {
         ]
     }
 }
-
--- metadata.py
+++ metadata.py
@@ -1,1 +1,10 @@
-
+@metadata_processor
+def add_gitea_user(metadata):
+    if node.has_bundle('users'):
+        metadata['users'][metadata.get('gitea', {}).get('user', 'gitea')] = {
+                'sudo': False,
+                'full_name': 'Gitea User',
+                'shell': '/bin/bash',
+                'home': '/var/lib/gitea',
+        }
+    return metadata, DONE
